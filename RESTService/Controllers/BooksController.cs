b8e1bf0fed482fbd2b90a2d994596af0cb572c1d
Gábor Tamás Tornai <gabo0127@edu.easj.dk>
2021-10-14 18:08:00 +0200

    Add project files.

---
+++ RESTService/Controllers/BooksController.cs
@@ -0,0 +1,55 @@
+﻿using ClassLibrary;
+using Microsoft.AspNetCore.Mvc;
+using RESTService.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RESTService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+
+        
+    {
+        
+        private BookManager _manager = new BookManager();
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GellAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            return "value";
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] Book value)
+        {
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] Book value)
+        {
+            _manager.
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RESTService/Managers/BookManager.cs
@@ -0,0 +1,53 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTService.Managers
+{
+    public class BookManager
+    {
+        private static List<Book> _book = new List<Book>()
+        {
+            new Book()
+            {Title= "DWEFR", Author="AAA",PageNumber=1234 , ISBN="EEFW2"  },
+
+            new Book()
+            {Title= "ASD", Author="BBB",PageNumber=4321 , ISBN="DFGTZ1"  }
+
+
+        };
+        public List<Book> GellAll()
+        {
+            return new List<Book>(_book);
+        }
+
+        public Book GetBookIds(String ISBN)
+        {
+            return   _book.Find(x => x.ISBN == ISBN);
+        }
+
+
+        public void PostBooks(Book NewBook)
+        {
+            _book.Add(NewBook);
+        }
+
+        public void Put(string ISBN, Book UpBook)
+        {
+            _book.u
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
